d1deedb9fc76e952075b8c86234c9a5c439740cc
juan pablo ospina <jampol_1@hotmail.com>
2018-11-12 22:07:10 -0500

    asta

--- src/aplicacion/Barrera.java
+++ src/aplicacion/Barrera.java
@@ -1,3 +1,15 @@
+package aplicacion;
+
+import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.event.*;
 
 public abstract class Barrera {
 
