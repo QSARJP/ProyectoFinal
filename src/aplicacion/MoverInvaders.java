f6d31135f82b68063ed51c090f4b80bfe47ebf03
Miguel Ángel <migue24co@gmail.com>
2018-11-28 01:36:09 -0500

    movimiento

--- src/aplicacion/MoverInvaders.java
+++ src/aplicacion/MoverInvaders.java
@@ -15,21 +15,20 @@ public class MoverInvaders extends Thread{
     @Override
     public void run(){
         int n = 1;
-        while (true){
-            for (int i = 0; i < space.getInvasores().size(); i++){
+        for(int j = 0; j < 50; j++){
+            for (String i: space.getInvasores().keySet()){
+                //System.out.println(space.getInvasores().get(i).getClass().getName()+" "+space.getInvasores().get(i).getPosicion());
                 Invasor invasor = space.getInvasores().get(i);
-                if (invasor.getPosicionInt()[0] >= 750 || invasor.getPosicionInt()[0] <= 50){
-                    n = n*-1;
-                }
-                space.mover(invasor, 3, 0);
+                space.mover(invasor, 2, 0);
             }
             esperar();
+            spaceGUI.refresque();
         }
     }
 
     private void esperar(){
         try{
-            Thread.sleep(1000);
+            Thread.sleep(10);
         }
         catch(InterruptedException e){
             Thread.currentThread().interrupt();
