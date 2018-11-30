950469051c591c4eb84bd4796fbaaa8c082a5563
juan pablo ospina <jampol_1@hotmail.com>
2018-11-30 11:28:00 -0500

    move

--- src/aplicacion/MoverInvaders.java
+++ src/aplicacion/MoverInvaders.java
@@ -20,21 +20,20 @@ public class MoverInvaders extends Thread{
             for (int i = 0;i< space.getInvasores().size();i++){
                 Invasor invasor = space.getInvasores().get(i);
                 space.mover(invasor, 1*n, 0);
-                //System.out.println(invasor.getClass().getName()+" "+invasor.getPosicionInt()[0]+" "+invasor.getPosicionInt()[1]);
-                if(invasor.getPosicionInt()[0]+50 == 750 || invasor.getPosicionInt()[0] == 0){
+                if(invasor.getPosicionInt()[0] == 750 || invasor.getPosicionInt()[0] == 0){
                     if (b){
-                        n = n*-1;
                         b = false;
                     }
 
                 }  
-            }
+            }  
             if (!b){
                 for (int j = 0;j< space.getInvasores().size();j++){
                     Invasor invasor2 = space.getInvasores().get(j);
                     space.mover(invasor2, 0, 5);
                 }
                 b=true;
+                n = n*-1;
             }
             esperar();
             spaceGUI.refresque();
